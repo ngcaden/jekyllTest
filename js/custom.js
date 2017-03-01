f4b8d0be9216631909282e6f8604acfa5bd53d5c
ngcaden <quangnguyen30795@gmail.com>
2017-03-01 04:43:59 +0000

    updated personal page

--- js/custom.js
+++ js/custom.js
@@ -1,18 +1,5 @@
-function myFunction() {
-	document.getElementById("myDropdown").classList.toggle("show");
-}
-
-
-window.onclick = function(event) {
-	if (!event.target.matches('.dropbtn')) {
-		var dropdowns =
-	document.getElementByClassName("drop-content");
-		var i;
-		for (i = 0; i< dropdowns.length; i++) {
-			var openDropdown = dropdowns[i];
-			if (openDropdown.classList.contains('show')) {
-				openDropdown.classList.remove('show');
-			}
-		}
-	}
-}
+$(document).ready(function(){
+	$('.trigger').on('click', function(){
+		$(this).toggleClass('clicked');
+	});
+});
